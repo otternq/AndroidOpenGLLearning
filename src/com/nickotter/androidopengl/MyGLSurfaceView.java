6171872249273f1f7b0bcae4f5d3dea485219555
Nick Otter <otternq@gmail.com>
2012-11-11 13:15:54 -0800

    initial commit and view set-up with opengl color changes

---
+++ src/com/nickotter/androidopengl/MyGLSurfaceView.java
@@ -0,0 +1,30 @@
+package com.nickotter.androidopengl;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class MyGLSurfaceView extends GLSurfaceView {
+	
+	private static final String LOG_TAG = MyGLSurfaceView.class.getSimpleName();
+    private DrawSomethingRenderer _renderer;
+
+	public MyGLSurfaceView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		
+		this._renderer = new DrawSomethingRenderer();
+		
+		setRenderer(this._renderer);
+	}
+	
+	public boolean onTouchEvent(final MotionEvent event) {
+	    queueEvent(new Runnable() {
+	        public void run() {
+	            _renderer.setColor(event.getX() / getWidth(), event.getY() / getHeight(), 1.0f);
+	        }
+	    });
+	    return true;
+	}
+
+}
